b5ccecb1136f598d4de9486c30c075228477a30d
Laggus <53914190+Laggus@users.noreply.github.com>
2019-12-20 23:34:40 +0100

    Initial commit
    - Basic playback with position slider and volume slider

---
+++ Audio Book Player/Audio Book Player/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Audio_Book_Player
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+			if (openFileDialog.ShowDialog() == true)
+			{
+
+
+				Player player = new Player();
+				player.MediaPlayer.Source = new Uri(openFileDialog.FileName);
+				player.Show();
+				this.Close();
+			}
+		}
+	}
+}
---
+++ Audio Book Player/Audio Book Player/Player.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace Audio_Book_Player
+{
+	/// <summary>
+	/// Interaction logic for Player.xaml
+	/// </summary>
+	public partial class Player : Window
+	{
+		DispatcherTimer timer = new DispatcherTimer();
+		double currentTime, totalTime;
+
+		public Player()
+		{
+			InitializeComponent();
+		}
+
+		private void Grid_Loaded(object sender, RoutedEventArgs e)
+		{
+
+			MediaPlayer.Play();
+		}
+
+		private void MediaPlayer_MediaOpened(object sender, RoutedEventArgs e)
+		{
+			if (MediaPlayer.NaturalDuration.HasTimeSpan)
+			{
+				totalTime = MediaPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+				TotalMin.Text = MediaPlayer.NaturalDuration.TimeSpan.Minutes.ToString();
+				TotalSec.Text = MediaPlayer.NaturalDuration.TimeSpan.Seconds.ToString();
+
+				PositionSlider.Maximum = totalTime;
+
+			}
+			VolumeSlider.Value = MediaPlayer.Volume;
+			timer.Interval = TimeSpan.FromSeconds(1);
+			timer.Tick += Tick;
+			timer.Start();
+		}
+
+		private void PositionSlider_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+		{
+			if (System.Windows.Input.Mouse.LeftButton == MouseButtonState.Released)
+				MediaPlayer.Play();
+			MediaPlayer.Position = new TimeSpan(0, 0, Convert.ToInt32(Math.Floor(PositionSlider.Value)));
+			currentTime = PositionSlider.Value;
+		}
+
+		private void PositionSlider_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (System.Windows.Input.Mouse.LeftButton == MouseButtonState.Pressed)
+						MediaPlayer.Pause();
+		}
+
+		private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			MediaPlayer.Volume = VolumeSlider.Value;
+		}
+
+		void Tick(object sender, EventArgs e)
+		{
+			currentTime = MediaPlayer.Position.TotalSeconds;
+			PositionSlider.Value = currentTime;
+			CurrentMin.Text = MediaPlayer.Position.Minutes.ToString();
+			CurrentSec.Text = MediaPlayer.Position.Seconds.ToString();
+		}
+
+	}
+}
